6d8d887846d1af860dde4e2fc66680689d66f40f
Gary Wilber <spacetech326@gmail.com>
2022-08-27 16:51:16 -0700

    Update for 2.12.0-beta.dev.20220827.4

--- src/Argus.ts
+++ src/Argus.ts
@@ -6,7 +6,7 @@ import { DamageType, EntityType } from "game/entity/IEntity";
 import { EquipType, SkillType } from "game/entity/IHuman";
 import { Game } from "game/Game";
 import { ItemType, ItemTypeGroup, RecipeLevel } from "game/item/IItem";
-import { RecipeComponent } from "game/item/Items";
+import { RecipeComponent } from "game/item/ItemDescriptions";
 import Mod from "mod/Mod";
 import Register, { Registry } from "mod/ModRegistry";
 import { RenderSource, UpdateRenderFlag } from "renderer/IRenderer";
